ace78c51223caa0eba750195e3e19f7dee91d132
isin75 <jon.s124524@gmail.com>
2022-10-09 19:36:41 +0300

    node, mongodb, mongoose, express / router, post, postController

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+import router from './router.js'
+
+const PORT = 5000
+const DB_URL = `mongodb+srv://isin:isin@cluster0.w7orbdc.mongodb.net/?retryWrites=true&w=majority`
+
+const app = express()
+
+app.use(express.json())
+app.use('/api', router)
+
+const startApp = async () => {
+    try {
+        await mongoose.connect(DB_URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+        app.listen(PORT, () => console.log(`Server start -> http://localhost:${PORT}/`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
